1f67be305f29594e2f63082ffd5649bea7e45294
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2025-12-18 01:04:04 +0300

    Update SleepTrackerApp.java

--- src/main/java/ru/yandex/practicum/sleeptracker/SleepTrackerApp.java
+++ src/main/java/ru/yandex/practicum/sleeptracker/SleepTrackerApp.java
@@ -9,11 +9,45 @@ import java.time.Period;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+// Класс-обёртка для результата анализа
+class SleepAnalysisResult<T> {
+    private String description;
+    private T value;
+    
+    public SleepAnalysisResult(String description, T value) {
+        this.description = description;
+        this.value = value;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+    
+    public T getValue() {
+        return value;
+    }
+    
+    @Override
+    public String toString() {
+        return String.format("%s: %s", description, value.toString());
+    }
+}
+
+// Функциональный интерфейс для анализа сна
+interface SleepAnalysisFunction extends Function<List<SleepingSession>, SleepAnalysisResult<?>> {
+    // Базовый метод по умолчанию для удобства
+    default String getName() {
+        return this.getClass().getSimpleName();
+    }
+}
+
 // Основной класс приложения
 public class SleepTrackerApp {
+
     private List<SleepAnalysisFunction> analysisFunctions;
     
     public SleepTrackerApp() {
