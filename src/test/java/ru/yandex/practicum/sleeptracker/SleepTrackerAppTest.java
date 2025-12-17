b501bdf7b4787d4d5b68e90aa220b84317cb0356
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2025-12-18 00:49:31 +0300

    Update SleepTrackerAppTest.java

--- src/test/java/ru/yandex/practicum/sleeptracker/SleepTrackerAppTest.java
+++ src/test/java/ru/yandex/practicum/sleeptracker/SleepTrackerAppTest.java
@@ -18,14 +18,14 @@ class SleepTrackerAppTest {
                               LocalDateTime.of(2025, 10, 3, 8, 0),
                               SleepQuality.NORMAL)
         );
-        
+
         SleepTrackerApp.TotalSessionsFunction function = new SleepTrackerApp.TotalSessionsFunction();
         SleepAnalysisResult<Integer> result = function.apply(sessions);
-        
+
         assertEquals(2, result.getValue());
         assertEquals("Общее количество сессий сна", result.getDescription());
     }
-    
+
     @Test
     void testMinDurationFunction() {
         List<SleepingSession> sessions = Arrays.asList(
@@ -36,13 +36,13 @@ class SleepTrackerAppTest {
                               LocalDateTime.of(2025, 10, 3, 15, 20),
                               SleepQuality.NORMAL)
         );
-        
+
         SleepTrackerApp.MinDurationFunction function = new SleepTrackerApp.MinDurationFunction();
         SleepAnalysisResult<Long> result = function.apply(sessions);
-        
+
         assertEquals(50L, result.getValue());
     }
-    
+
     @Test
     void testMaxDurationFunction() {
         List<SleepingSession> sessions = Arrays.asList(
@@ -53,13 +53,13 @@ class SleepTrackerAppTest {
                               LocalDateTime.of(2025, 10, 3, 10, 0),
                               SleepQuality.NORMAL)
         );
-        
+
         SleepTrackerApp.MaxDurationFunction function = new SleepTrackerApp.MaxDurationFunction();
         SleepAnalysisResult<Long> result = function.apply(sessions);
-        
+
         assertEquals(660L, result.getValue());
     }
-    
+
     @Test
     void testAverageDurationFunction() {
         List<SleepingSession> sessions = Arrays.asList(
@@ -70,13 +70,13 @@ class SleepTrackerAppTest {
                               LocalDateTime.of(2025, 10, 3, 7, 0),
                               SleepQuality.NORMAL)
         );
-        
+
         SleepTrackerApp.AverageDurationFunction function = new SleepTrackerApp.AverageDurationFunction();
         SleepAnalysisResult<Double> result = function.apply(sessions);
-        
+
         assertEquals(480.0, result.getValue());
     }
-    
+
     @Test
     void testBadQualitySessionsFunction() {
         List<SleepingSession> sessions = Arrays.asList(
@@ -90,13 +90,13 @@ class SleepTrackerAppTest {
                               LocalDateTime.of(2025, 10, 4, 6, 20),
                               SleepQuality.BAD)
         );
-        
+
         SleepTrackerApp.BadQualitySessionsFunction function = new SleepTrackerApp.BadQualitySessionsFunction();
         SleepAnalysisResult<Long> result = function.apply(sessions);
-        
+
         assertEquals(1L, result.getValue());
     }
-    
+
     @Test
     void testSleeplessNightsFunction() {
         List<SleepingSession> sessions = Arrays.asList(
@@ -113,13 +113,13 @@ class SleepTrackerAppTest {
                               LocalDateTime.of(2025, 10, 5, 6, 20),
                               SleepQuality.BAD)
         );
-        
+
         SleepTrackerApp.SleeplessNightsFunction function = new SleepTrackerApp.SleeplessNightsFunction();
         SleepAnalysisResult<Long> result = function.apply(sessions);
-        
+
         assertEquals(1L, result.getValue());
     }
-    
+
     @Test
     void testSleeplessNightsWithEdgeCases() {
         List<SleepingSession> sessions = Arrays.asList(
@@ -130,13 +130,13 @@ class SleepTrackerAppTest {
                               LocalDateTime.of(2025, 10, 2, 23, 30),
                               SleepQuality.NORMAL)
         );
-        
+
         SleepTrackerApp.SleeplessNightsFunction function = new SleepTrackerApp.SleeplessNightsFunction();
         SleepAnalysisResult<Long> result = function.apply(sessions);
-        
+
         assertNotNull(result.getValue());
     }
-    
+
     @Test
     void testChronotypeFunctionOwl() {
         List<SleepingSession> sessions = Arrays.asList(
@@ -147,13 +147,13 @@ class SleepTrackerAppTest {
                               LocalDateTime.of(2025, 10, 2, 10, 0),
                               SleepQuality.NORMAL)
         );
-        
+
         SleepTrackerApp.ChronotypeFunction function = new SleepTrackerApp.ChronotypeFunction();
         SleepAnalysisResult<String> result = function.apply(sessions);
-        
+
         assertEquals("Сова", result.getValue());
     }
-    
+
     @Test
     void testChronotypeFunctionLark() {
         List<SleepingSession> sessions = Arrays.asList(
@@ -164,13 +164,13 @@ class SleepTrackerAppTest {
                               LocalDateTime.of(2025, 10, 3, 5, 45),
                               SleepQuality.NORMAL)
         );
-        
+
         SleepTrackerApp.ChronotypeFunction function = new SleepTrackerApp.ChronotypeFunction();
         SleepAnalysisResult<String> result = function.apply(sessions);
-        
+
         assertEquals("Жаворонок", result.getValue());
     }
-    
+
     @Test
     void testChronotypeFunctionPigeon() {
         List<SleepingSession> sessions = Arrays.asList(
@@ -181,10 +181,10 @@ class SleepTrackerAppTest {
                               LocalDateTime.of(2025, 10, 3, 6, 30),
                               SleepQuality.NORMAL)
         );
-        
+
         SleepTrackerApp.ChronotypeFunction function = new SleepTrackerApp.ChronotypeFunction();
         SleepAnalysisResult<String> result = function.apply(sessions);
-        
+
         assertEquals("Голубь", result.getValue());
     }
 }
