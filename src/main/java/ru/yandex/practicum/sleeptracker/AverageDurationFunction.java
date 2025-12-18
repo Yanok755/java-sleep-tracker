e5ba770b29fc564b871b3d4e5c561038357b9826
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2025-12-18 21:13:12 +0300

    Create AverageDurationFunction.java

---
+++ src/main/java/ru/yandex/practicum/sleeptracker/AverageDurationFunction.java
@@ -0,0 +1,22 @@
+package ru.yandex.practicum.sleeptracker;
+
+import java.util.List;
+
+// Функция 4: Средняя продолжительность сессии
+public class AverageDurationFunction implements SleepAnalysisFunction {
+    private static final String ANALYSIS_NAME = "Средняя продолжительность сессии (минут)";
+    private static final int DECIMAL_PLACES = 2;
+
+    @Override
+    public SleepAnalysisResult<Double> apply(List<SleepingSession> sessions) {
+        Double averageDuration = sessions.stream()
+            .mapToLong(SleepingSession::getDurationInMinutes)
+            .average()
+            .orElse(0.0);
+
+        double roundedAverage = Math.round(averageDuration * Math.pow(10, DECIMAL_PLACES)) 
+                              / Math.pow(10, DECIMAL_PLACES);
+
+        return new SleepAnalysisResult<>(ANALYSIS_NAME, roundedAverage);
+    }
+}
